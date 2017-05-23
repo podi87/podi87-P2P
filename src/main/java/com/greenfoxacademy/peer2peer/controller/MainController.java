219ea34244200951bc80651a6b2bd5bab707b34c
Andras Podobni <podobni.andras@gmail.com>
2017-05-23 21:44:51 +0200

    try to send a message to somebody using resttemplate

--- src/main/java/com/greenfoxacademy/peer2peer/controller/MainController.java
+++ src/main/java/com/greenfoxacademy/peer2peer/controller/MainController.java
@@ -2,9 +2,7 @@ package com.greenfoxacademy.peer2peer.controller;
 
 
 import com.greenfoxacademy.peer2peer.Service.ValidatorService;
-import com.greenfoxacademy.peer2peer.model.LogMessage;
-import com.greenfoxacademy.peer2peer.model.Message;
-import com.greenfoxacademy.peer2peer.model.UserName;
+import com.greenfoxacademy.peer2peer.model.*;
 import com.greenfoxacademy.peer2peer.repository.MessageRepository;
 import com.greenfoxacademy.peer2peer.repository.UserNameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 
 @Controller
@@ -26,22 +25,23 @@ public class MainController {
   private ValidatorService validatorService;
 
  public String error = "";
-  String env = System.getenv("CHAT_APP_LOGLEVEL");
+ private String envLog = System.getenv("CHAT_APP_LOGLEVEL");
+ private String envUrl = System.getenv("CHAT_APP_PEER_ADDRESS") + "/api/message/receive";
+ private String envID = System.getenv("CHAT_APP_UNIQUE_ID");
 
  public void logging(LogMessage logMessage) {
-   System.out.println(logMessage.getTimeLog()+"  " +env+logMessage.getRequest()+logMessage.getPath()
+   System.out.println(logMessage.getTimeLog()+"  " + envLog +logMessage.getRequest()+logMessage.getPath()
            +logMessage.getMethod()+logMessage.getParam());
  }
 
   @RequestMapping(value = "/")
   public String start(Model model) {
-    System.out.println(userNameRepository.count());
     model.addAttribute("users", userNameRepository.findAll());
     model.addAttribute("messages", messageRepository.findAllByOrderByTimestampAsc());
     model.addAttribute("actualName", userNameRepository.findOne(1l));
-    if(env.equals("INFO")){
+    if(envLog.equals("INFO")){
       logging(new LogMessage(" / ", " GET ", ""));
-    } else if (env.equals("ERROR")){
+    } else if (envLog.equals("ERROR")){
       System.err.println();
     }
     if (userNameRepository.count()==0) {
@@ -64,9 +64,9 @@ public class MainController {
       userNameRepository.save(new UserName(newName));
       temp = "redirect:/";
     }
-    if(env.equals("INFO")){
+    if(envLog.equals("INFO")){
       logging(new LogMessage(" /enter", " POST ", "newName: " + newName));
-    } else if (env.equals("ERROR")){
+    } else if (envLog.equals("ERROR")){
       System.err.println();
     }
    return temp;
@@ -76,9 +76,9 @@ public class MainController {
   public String update(Model model, @RequestParam(name = "id") long id, @RequestParam(name = "changedName") String changedName){
     userNameRepository.findOne(id).setName(changedName);
     userNameRepository.save(userNameRepository.findOne(id));
-    if(env.equals("INFO")){
+    if(envLog.equals("INFO")){
       logging(new LogMessage(" /update", " POST ", "changedName: " + changedName));
-    } else if (env.equals("ERROR")){
+    } else if (envLog.equals("ERROR")){
       System.err.println();
     }
     return "redirect:/";
@@ -86,10 +86,13 @@ public class MainController {
 
   @RequestMapping(value = "/send", method = RequestMethod.POST)
   public String chatPost(Model model, @RequestParam(name = "message") String message, @RequestParam(name = "id") long id) {
-    messageRepository.save(new Message(userNameRepository.findOne(id).getName(), message));
-    if(env.equals("INFO")){
+    Message receivedMessage = new Message(userNameRepository.findOne(id).getName(), message);
+    messageRepository.save(receivedMessage);
+    RestTemplate restTemplate = new RestTemplate();
+    restTemplate.postForObject(envUrl, new Received(receivedMessage, new Client(envID)), Received.class);
+    if(envLog.equals("INFO")){
       logging(new LogMessage(" /send ", " POST ", "message: " + message + " id: " + String.valueOf(id)));
-    } else if (env.equals("ERROR")){
+    } else if (envLog.equals("ERROR")){
       System.err.println();
     }
     return "redirect:/";
